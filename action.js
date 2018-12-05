baed8209414ef0ea3fe7bca6f9c3390a4b0d8054
Kristy Chen <kristyyy@Kristys-MacBook-Pro.local>
2018-12-04 16:47:48 -0800

    Complete Nav and Intro.

---
+++ action.js
@@ -0,0 +1,24 @@
+"use strict";
+
+(function() {
+    
+    function qs(selector) {
+        return document.querySelector(selector);
+    }
+    
+    window.onload = function() {
+      
+	  };
+
+    function checkClick() {
+      
+    
+    }
+
+    function test() {
+    	alert("yo");
+    }
+    
+    
+    
+})();
